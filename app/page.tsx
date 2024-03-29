d55e506c5321509ab10425aea27eec5685d4b652
Prathya Thakur <98422626+thakurprathya@users.noreply.github.com>
2024-03-30 01:07:58 +0530

    updating readme & fixing width error....

--- app/page.tsx
+++ app/page.tsx
@@ -65,6 +65,9 @@ export default function Home() {
     };
 
     useEffect(() => {
+        setScreenWidth(window?.innerWidth);
+    },[]);
+    useEffect(() => {
         const handleResize = () => { setScreenWidth(window.innerWidth) };
         window.addEventListener("resize", handleResize);
         return () => { window.removeEventListener("resize", handleResize) };

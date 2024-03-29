710e91c5dda3e7f5fbaa1d123aadb63d10ed9c6a
Prathya Thakur <98422626+thakurprathya@users.noreply.github.com>
2024-03-30 00:54:46 +0530

    added responsiveness....

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { cardData, ticketData, advertismentData } from "@/constants";
 import HeadCard from "@/components/HeadCard";
 import Advertisment from "@/components/Advertisment";
@@ -10,54 +10,80 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 interface HomeProps {
-    theme: string;
+    screenWidth: number;
 }
 
-const CustomPrevArrow: React.FC<HomeProps & { onClick: React.MouseEventHandler<HTMLButtonElement> }> = ({ theme, onClick }) => (
-    <button onClick={onClick} className={`absolute top-[50%] -left-10 flex items-center justify-center w-[37px] h-[49px] slick-arrow custom-prev-arrow border border-[#2C9CF0] text-[#2C9CF0] hover:bg-[#2C9CF0] hover:text-white`}>
+const CustomPrevArrow: React.FC<HomeProps & { onClick: React.MouseEventHandler<HTMLButtonElement> }> = ({ screenWidth, onClick }) => (
+    <button onClick={onClick} className={`absolute top-[50%] -left-8 md:-left-10 flex items-center justify-center slick-arrow custom-prev-arrow border border-[#2C9CF0] text-[#2C9CF0] hover:bg-[#2C9CF0] hover:text-white ${screenWidth<=380 ? 'w-[25px] h-[37px]' : 'w-[37px] h-[49px]'}`}>
         {`<`}
     </button>
 );
 
-const CustomNextArrow: React.FC<HomeProps & { onClick: React.MouseEventHandler<HTMLButtonElement> }> = ({ theme, onClick }) => (
-    <button onClick={onClick} className={`absolute top-[50%] -right-10 flex items-center justify-center w-[37px] h-[49px] slick-arrow custom-next-arrow border border-[#2C9CF0] text-[#2C9CF0] hover:bg-[#2C9CF0] hover:text-white`}>
+const CustomNextArrow: React.FC<HomeProps & { onClick: React.MouseEventHandler<HTMLButtonElement> }> = ({ screenWidth, onClick }) => (
+    <button onClick={onClick} className={`absolute top-[50%] -right-8 md:-right-10 flex items-center justify-center slick-arrow custom-next-arrow border border-[#2C9CF0] text-[#2C9CF0] hover:bg-[#2C9CF0] hover:text-white ${screenWidth<=380 ? 'w-[25px] h-[37px]' : 'w-[37px] h-[49px]'}`}>
         {`>`}
     </button>
 );
 
 export default function Home() {
     const [theme, setTheme] = useState('light');
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
     const sliderRef = useRef<Slider>(null);
+    
+    const HandleThemeChange = ()=>{
+        theme === 'light' ? setTheme('dark') : setTheme('light');
+    }
+    const slides = ()=>{
+        if(screenWidth > 1300) return 3;
+        else if(screenWidth > 850) return 2;
+        return 1;
+    }
+    const renderCardData = () => {
+        if (screenWidth > 1300) {
+            return cardData.map((card, index) =>
+                <HeadCard key={index} cardData={card} theme={theme} />
+            );
+        } else if(screenWidth > 1100){
+            return cardData.slice(0, 3).map((card, index) =>
+                <HeadCard key={index} cardData={card} theme={theme} />
+            );
+        } else {
+            return cardData.slice(0, 2).map((card, index) =>
+                <HeadCard key={index} cardData={card} theme={theme} />
+            );
+        }
+    };
+    
     const settings = {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: slides(),
         slidesToScroll: 1,
-        prevArrow: <CustomPrevArrow theme={theme} onClick={() => sliderRef.current?.slickPrev()} />,
-        nextArrow: <CustomNextArrow theme={theme} onClick={() => sliderRef.current?.slickNext()} />
+        prevArrow: <CustomPrevArrow screenWidth={screenWidth} onClick={() => sliderRef.current?.slickPrev()} />,
+        nextArrow: <CustomNextArrow screenWidth={screenWidth} onClick={() => sliderRef.current?.slickNext()} />
     };
 
-    const HandleThemeChange = ()=>{
-        theme === 'light' ? setTheme('dark') : setTheme('light');
-    }
+    useEffect(() => {
+        const handleResize = () => { setScreenWidth(window.innerWidth) };
+        window.addEventListener("resize", handleResize);
+        return () => { window.removeEventListener("resize", handleResize) };
+    }, []);
 
     return (
         <>
-            <button onClick={HandleThemeChange} className={`fixed top-5 md:top-10 right-5 md:right-8 w-8 h-8 md:w-10 md:h-10`}>
+            <button onClick={HandleThemeChange} className={`fixed top-5 md:top-10  lg:right-8 md:w-10 md:h-10 ${screenWidth<=350 ? 'w-7 h-7 right-2' : 'w-8 h-8 right-5'}`}>
                 <svg viewBox="0 0 24 24" fill="currentColor" className={`${theme === 'light' ? 'text-black' : 'text-[#ffffff]'}`} >
                     <path d="M7.5 2c-1.79 1.15-3 3.18-3 5.5s1.21 4.35 3.03 5.5C4.46 13 2 10.54 2 7.5A5.5 5.5 0 017.5 2m11.57 1.5l1.43 1.43L4.93 20.5 3.5 19.07 19.07 3.5m-6.18 2.43L11.41 5 9.97 6l.42-1.7L9 3.24l1.75-.12.58-1.65L12 3.1l1.73.03-1.35 1.13.51 1.67m-3.3 3.61l-1.16-.73-1.12.78.34-1.32-1.09-.83 1.36-.09.45-1.29.51 1.27 1.36.03-1.05.87.4 1.31M19 13.5a5.5 5.5 0 01-5.5 5.5c-1.22 0-2.35-.4-3.26-1.07l7.69-7.69c.67.91 1.07 2.04 1.07 3.26m-4.4 6.58l2.77-1.15-.24 3.35-2.53-2.2m4.33-2.7l1.15-2.77 2.2 2.54-3.35.23m1.15-4.96l-1.14-2.78 3.34.24-2.2 2.54M9.63 18.93l2.77 1.15-2.53 2.19-.24-3.34z" />
                 </svg>
             </button>
 
-            <div className={`p-5 md:py-10 md:px-20 w-full h-full flex flex-col items-center ${theme === 'light' ? 'bg-[#F7F7F8]' : 'bg-[#292B32]'}`}>
+            <div className={`p-5 md:py-10 md:px-10 lg:px-20 w-full h-full flex flex-col items-center ${theme === 'light' ? 'bg-[#F7F7F8]' : 'bg-[#292B32]'}`}>
                 <div className={`flex flex-col`}>
                     <p className={`font-poppins font-[700] text-[24px] leading-[36px] border-b-2 border-[#738FFF] w-[81px] ${theme === 'light' ? 'text-black' : 'text-white'}`}>Sports</p>
                     <div className={`mt-[20px] flex gap-4 flex-wrap justify-center`}>
                         {/* cards */}
-                        {cardData.map((card, index) =>
-                            <HeadCard key={index} cardData={card} theme={theme} />
-                        )}
+                        {renderCardData()}
 
                         {/* advertisment */}
                         <Advertisment adData={advertismentData} theme={theme} />
@@ -66,12 +92,12 @@ export default function Home() {
                 </div>
 
                 <div className={`mt-[50px] w-full flex flex-col items-center p-10 bg-gradient-to-b ${theme === 'light' ? 'text-black from-[#F9F8FF] to-[#F3F9FF]' : 'text-[#ffffff] from-[#18282A] to-[#221A2C]'}`}>
-                    <p className={`font-poppins font-[700] text-[50px] leading-[53px]`}>Collection Spotlight</p>
-                    <p className={`mt-[20px] max-w-[917px] text-center font-[400] text-[14px] leading-[23px]`}>Discover extraordinary moments with our Spotlight Collection metatickets—exclusive access to premium events for an unforgettable experience. Grab yours today!</p>
-                    <div className={`mt-[30px] w-[90%] md:w-[78%]`}>
+                    <p className={`text-center font-poppins font-[700] text-[40px] md:text-[50px] leading-[42px] md:leading-[53px]`}>Collection Spotlight</p>
+                    <p className={`mt-[20px] max-w-[917px] text-center font-[400] text-[12px] md:text-[14px] leding-[17px] md:leading-[23px]`}>Discover extraordinary moments with our Spotlight Collection metatickets—exclusive access to premium events for an unforgettable experience. Grab yours today!</p>
+                    <div className={`mt-[30px] ${screenWidth <= 350 ? 'w-[99%]' : 'w-[90%]'} md:w-[85%] lg:w-[78%]`}>
                         <Slider ref={sliderRef} {...settings}>
                             {ticketData.map((ticket, index)=>
-                                <Ticket key={index} ticketData={ticket} theme={theme} />
+                                <Ticket key={index} ticketData={ticket} theme={theme} screenWidth={screenWidth} />
                             )}
                         </Slider>
                     </div>
--- components/Ticket.tsx
+++ components/Ticket.tsx
@@ -11,25 +11,34 @@ interface Props {
         btnText: string;
     };
     theme: string;
+    screenWidth: number;
 };
 
-const Ticket = ({ticketData, theme} : Props) => {
+const Ticket = ({ticketData, theme, screenWidth} : Props) => {
     return (
-        <div className={`w-[257px] h-[625px] flex flex-col items-center p-2 shadow-xl ${theme === 'light' ? 'bg-[#ffffff]' : 'bg-[#3B3E47]'}`}>
-            <Image src={ticketData?.img} width={257} height={401} alt="ticket"/>
-            <div className={`-mt-[12px] relative`}>
-                <div className={`absolute top-0 -left-10 rounded-full w-5 h-5 ${theme === 'light' ? 'bg-[#f0f6fc]' : 'bg-[#221A2C]'}`}></div>
-                <p className={`text-[#A9ACB2]`}>- - - - - - - - - - - - - - - - - </p>
-                <div className={`absolute top-0 -right-10 rounded-full w-5 h-5 ${theme === 'light' ? 'bg-[#f0f6fc]' : 'bg-[#221A2C]'}`}></div>
+        <div className={`w-[215px] h-[510px] md:w-[257px] md:h-[625px] flex flex-col items-center p-2 shadow-lg md:shadow-xl ${theme === 'light' ? 'bg-[#ffffff]' : 'bg-[#3B3E47]'}`}>
+            <Image src={ticketData?.img} width={screenWidth<=380 ? 215 : 257} height={screenWidth<=380 ? 375 : 401} alt="ticket"/>
+            <div className={`-mt-[15px] md:-mt-[12px] relative`}>
+                <div className={`absolute top-0 -left-8 md:-left-10 rounded-full w-5 h-5 ${theme === 'light' ? 'bg-[#f0f6fc]' : 'bg-[#221A2C]'}`}></div>
+                <p className={`text-[#A9ACB2]`}>
+                    {screenWidth <= 330 ?
+                        `- - - - - - - - - - - - -`
+                    : screenWidth <= 768 ?
+                        `- - - - - - - - - - - - - - -`
+                    :
+                        `- - - - - - - - - - - - - - - - -`
+                    }
+                </p>
+                <div className={`absolute top-0 -right-8 md:-right-10 rounded-full w-5 h-5 ${theme === 'light' ? 'bg-[#f0f6fc]' : 'bg-[#221A2C]'}`}></div>
             </div>
-            <p className={`mt-[20px] font-poppins font-[500] text-[17px] leading-[27px] ${theme === 'light' ? 'text-black' : 'text-[#ffffff]'}`}>{ticketData?.team}</p>
-            <div className={`mt-[10px] flex items-center gap-2 uppercase font-poppins font-[400] text-[14px] leading-[21px] ${theme === 'light' ? 'text-black' : 'text-[#ffffff]'}`}>
+            <p className={`mt-[20px] font-poppins font-[500] text-[15px] md:text-[17px] leading-[22px] md:leading-[27px] ${theme === 'light' ? 'text-black' : 'text-[#ffffff]'}`}>{ticketData?.team}</p>
+            <div className={`mt-[10px] flex items-center gap-1 md:gap-2 uppercase font-poppins font-[400] text-[12px] md:text-[14px] leading-[18px] md:leading-[21px] ${theme === 'light' ? 'text-black' : 'text-[#ffffff]'}`}>
                 <p>{ticketData?.date+" |"}</p>
                 <p>{ticketData?.day+" |"}</p>
                 <p>{ticketData?.time}</p>
             </div>
-            <p className={`mt-[20px] capitalize text-center w-[213px] font-[400] text-[14px] leading-[21px] ${theme === 'light' ? 'text-[#525965]' : 'text-[#dfdfdf]'}`}>{ticketData?.location}</p>
-            <button className={`mt-[20px] w-[213px] h-[36px] bg-[#1D1D1F] text-white font-[500] text-[13px] leading-[15px]`}>{ticketData?.btnText}</button>
+            <p className={`mt-[20px] capitalize text-center md:w-[213px] font-[400] text-[12px] md:text-[14px] leading-[18px] md:leading-[21px] ${theme === 'light' ? 'text-[#525965]' : 'text-[#dfdfdf]'}`}>{ticketData?.location}</p>
+            <button className={`mt-[20px] w-[180px] md:w-[213px] h-[36px] bg-[#1D1D1F] text-white font-[500] text-[13px] leading-[15px]`}>{ticketData?.btnText}</button>
         </div>
     )
 };
